1fff27c3e1ce2ffff4482b1355ee1f87ed36f1f4
duyle <duy.leduc8@gmail.com>
2020-04-11 12:11:26 +0700

    delete btn functionality

--- app.js
+++ app.js
@@ -59,6 +59,22 @@ var budgetController = (function () {
       return newItem;
     },
 
+    deleteItem: function (type, id) {
+      var ids, index;
+
+      //Populate an array contain all [type].id
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+
+      //get index of the id
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function () {
       //Calculate total income and exspense
       calculateTotal("exp");
@@ -105,6 +121,7 @@ var UIController = (function () {
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
+    container: ".container",
   };
 
   return {
@@ -128,12 +145,12 @@ var UIController = (function () {
         element = DOMstrings.incomeContainer;
 
         html =
-          '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">+ %value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+          '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">+ %value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
       } else if (type === "exp") {
         element = DOMstrings.expensesContainer;
 
         html =
-          '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">- %value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+          '<div class="item clearfix" id="exp-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">- %value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
       }
 
       //Replace the placeholder text with actual data
@@ -143,6 +160,11 @@ var UIController = (function () {
 
       //Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+
+    deleteListItem: function (selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearField: function () {
@@ -192,6 +214,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.container)
+      .addEventListener("click", ctrlDeleteItem);
   };
 
   var updateBudget = function () {
@@ -227,6 +253,28 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
   };
 
+  var ctrlDeleteItem = function (event) {
+    var itemID, splitID, type, id;
+
+    //Get selected ID when click to delete item btn using event delegation, target element and DOM traversal
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      splitID = itemID.split("-");
+      type = splitID[0];
+      id = parseInt(splitID[1]);
+
+      //1. Delete the item from the data structure
+      budgetCtrl.deleteItem(type, id);
+
+      //2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
+
+      //3. Update and show the new budget
+      updateBudget();
+    }
+  };
+
   return {
     init: function () {
       console.log("Application has started");
@@ -234,7 +282,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         budget: 0,
         totalInc: 0,
         totalExp: 0,
-        percentage: 0,
+        percentage: -1,
       });
       setupEventListeners();
     },
